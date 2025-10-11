6fb93cc5db9c2458c7cbb08a580e7de03f90aae7
Tanish-max915 <jacob.lancastercity@gmail.com>
2025-10-11 13:44:10 +0530

    Added coins delete [No score} + killzone [no code]

---
+++ scripts/kz.gd
@@ -0,0 +1,7 @@
+extends Area2D
+
+@onready var d_delay: Timer = $d_delay
+
+#We use a timer for dealy b/w death and respawn
+func _on_body_entered(body: Node2D) -> void:
+	pass # Replace with function body.
