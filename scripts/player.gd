18277cee3a166ff838a7a8300bb81b8500a51f4e
Tanish-max915 <jacob.lancastercity@gmail.com>
2025-10-14 22:11:58 +0530

    Directions work
    You can stand left now

--- scripts/player.gd
+++ scripts/player.gd
@@ -20,5 +20,9 @@ func _physics_process(delta: float) -> void:
 		velocity.x = direction * SPEED # Left
 	else: 
 		velocity.x = move_toward(velocity.x, 0, SPEED) # Right
+	
+	if velocity.x:
+		$AnimatedSprite2D.flip_h = velocity.x < 0
+
 
 	move_and_slide()
