2810a9fb72312ed4e631c03852e95af7ef6db98c
Tanish-max915 <jacob.lancastercity@gmail.com>
2025-10-16 19:59:01 +0530

    Added dash and double jump
    requesting dash particles

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,8 +1,11 @@
 extends CharacterBody2D
 
-const SPEED = 160
+@export_subgroup("Settings")
+const SPEED = 135
 const JUMP_VELOCITY = -300.00
-
+var jump_change = 0
+var dash_speed = 2
+var dashing = false
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -10,15 +13,31 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and jump_change == 1:
+		velocity.y = JUMP_VELOCITY
+		$Jump.play()
+		jump_change = 2
+	
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 		$Jump.play()
+		jump_change = 1
+	
+	if !Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		jump_change = 0
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("left", "right")
+	
+	if Input.is_action_pressed("dash"):
+		if !dashing and direction:
+			start_dash()
 	if direction:
-		velocity.x = direction * SPEED # Left
+		if dashing:
+			velocity.x = direction * SPEED * dash_speed # Left
+		else:
+			velocity.x = direction * SPEED
 	else: 
 		velocity.x = move_toward(velocity.x, 0, SPEED) # Right
 	
@@ -27,3 +46,10 @@ func _physics_process(delta: float) -> void:
 
 
 	move_and_slide()
+
+func start_dash():
+	dashing = true
+	$dash_time.start()
+
+func stop_dash():
+	dashing = false
